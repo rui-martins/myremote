0f20f3ce0bea59a889f4b64fc89d89ae9a5ec29b
Nuno Fachada <nuno.fachada@ulusofona.pt>
2018-04-24 15:16:58 +0100

    Convert program to be a bit more object-oriented

--- TestProjectForGit/Program.cs
+++ TestProjectForGit/Program.cs
@@ -6,6 +6,12 @@ namespace TestProjectForGit
     {
         static void Main(string[] args)
         {
+            Program prog = new Program();
+            prog.PrintSomething();
+        }
+
+        private void PrintSomething()
+        {
             Console.WriteLine("I'm a test for git tags");
             Console.WriteLine("Tags are a good way to remember versions");
             Console.ReadKey();
