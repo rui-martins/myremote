72002a788e408c20a52cd3dbc48df3954c2489c7
Nuno Fachada <nuno.fachada@ulusofona.pt>
2018-04-24 15:13:04 +0100

    Add new message specifying what git tags are good for

--- TestProjectForGit/Program.cs
+++ TestProjectForGit/Program.cs
@@ -7,6 +7,7 @@ namespace TestProjectForGit
         static void Main(string[] args)
         {
             Console.WriteLine("I'm a test for git tags");
+            Console.WriteLine("Tags are a good way to remember versions");
         }
     }
 }
