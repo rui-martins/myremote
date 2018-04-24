ffbf6efbc16f41b8f6a39cf53d7c24aa855028d4
Nuno Fachada <nuno.fachada@ulusofona.pt>
2018-04-24 15:19:38 +0100

    Add yet more comments

--- TestProjectForGit/Program.cs
+++ TestProjectForGit/Program.cs
@@ -4,14 +4,17 @@ using System;
 // The namespace we put our program in
 namespace TestProjectForGit
 {
+    // Our class is called Program, we should change it some day
     class Program
     {
+        // This is the old boring main method
         static void Main(string[] args)
         {
             Program prog = new Program();
             prog.PrintSomething();
         }
 
+        // And this is an instance method
         private void PrintSomething()
         {
             Console.WriteLine("I'm a test for git tags");
